c49ba52c41bb7927a8a33b0f93bee0b3174bfdbd
tivt <tivt231@gmail.com>
2024-02-19 13:46:41 -0300

    day4 part1

---
+++ day4/main.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "day4/input"
+#define TEST_INPUT "day4/test_input"
+
+typedef struct {
+  int *arr;
+  size_t pos;
+  size_t len;
+} Bucket;
+
+typedef struct {
+  Bucket **buckets;
+  size_t pos;
+  size_t len;
+} Set;
+
+Set *set_create(void) {
+  Set *set = (Set *)malloc(sizeof(Set));
+  if (set == NULL) {
+    perror("malloc set");
+    return NULL;
+  }
+
+  set->len = 16;
+  set->pos = 0;
+  set->buckets = (Bucket **)malloc(sizeof(Bucket *) * set->len);
+  if (set->buckets == NULL) {
+    perror("malloc set->buckets");
+    return NULL;
+  }
+
+  for (int i = 0; i < (set->len); i++) {
+    Bucket *b = (Bucket *)malloc(sizeof(Bucket));
+    if (b == NULL) {
+      perror("malloc b");
+      return NULL;
+    }
+    b->pos = 0;
+    b->len = 128;
+    b->arr = (int *)malloc(sizeof(int) * b->len);
+    if (b->arr == NULL) {
+      perror("malloc b->arr");
+      return NULL;
+    }
+    set->buckets[i] = b;
+  }
+  return set;
+}
+
+void set_destroy(Set *set) {
+  for (int i = 0; i < (set->len); i++) {
+    Bucket *b = set->buckets[i];
+    free(b->arr);
+    free(b);
+  }
+  free(set->buckets);
+  free(set);
+}
+
+size_t set_hash(Set *set, int val) { return val % set->len; }
+
+int set_add(Set *set, int val) {
+  size_t idx = set_hash(set, val);
+  Bucket *b = set->buckets[idx];
+  if (b->pos < (b->len - 1)) {
+    b->arr[b->pos++] = val;
+    return 0;
+  }
+  return -1;
+}
+
+int set_has(Set *set, int val) {
+  size_t idx = set_hash(set, val);
+  Bucket *b = set->buckets[idx];
+  if (b->pos == 0) {
+    return 0;
+  } else {
+    for (int i = 0; i < (b->pos); i++) {
+      if (b->arr[i] == val) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+void set_print(Set *set) {
+  for (int i = 0; i < (set->len); i++) {
+    Bucket *b = set->buckets[i];
+    for (int j = 0; j < (b->pos); j++) {
+      printf("%d\n", b->arr[j]);
+    }
+  }
+}
+
+int main(void) {
+  FILE *file = fopen(INPUT, "r");
+  if (file == NULL) {
+    perror("fopen");
+    return 1;
+  }
+
+  int sum = 0;
+
+  char line[1024];
+  while (fgets(line, sizeof(line), file) != NULL) {
+    Set *set = set_create();
+    char *saveptr1, *saveptr2, *saveptr3;
+
+    char *id = strtok_r(line, ":", &saveptr1);
+    id = strtok_r(NULL, "", &saveptr1);
+    char *nums = strtok_r(id, "|", &saveptr2);
+    char *win_nums = strtok_r(NULL, "", &saveptr2);
+
+    char *num = strtok_r(nums, " ", &saveptr3);
+    while (num != NULL) {
+      set_add(set, atoi(num));
+
+      num = strtok_r(NULL, " ", &saveptr3);
+    }
+
+    int points = 0;
+    char *win_num = strtok_r(win_nums, " ", &saveptr3);
+    while (win_num != NULL) {
+      if (set_has(set, atoi(win_num))) {
+        if (points == 0) {
+          points = 1;
+        } else {
+          points *= 2;
+        }
+      }
+      win_num = strtok_r(NULL, " ", &saveptr3);
+    }
+
+    // set_print(set);
+    sum += points;
+  }
+
+  printf("answer: %d\n", sum);
+
+  return 0;
+}
