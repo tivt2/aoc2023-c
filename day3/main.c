62c9711331bc89601034907f0a254b00d9558186
tivt <tivt231@gmail.com>
2024-02-19 11:36:25 -0300

    day3 part2

--- day3/main.c
+++ day3/main.c
@@ -13,59 +13,139 @@
 
 #define is_symbol(char) (char != NOT_SYMBOL && !isdigit(char))
 
-int is_adjacent_to_symbol(int start, int end, char rows[3][1024]) {
-  char *middle_row = rows[1];
-  start = max(start - 1, 0);
-  end = min(end + 1, strlen(middle_row) - 2); // -2 to remove '\n'
-
-  if (start > 0) {
-    char middle_left = middle_row[start];
-    if (is_symbol(middle_left)) {
-      return 1;
-    }
-  }
-  if (end < strlen(middle_row) - 1) {
-    char middle_right = middle_row[end];
-    if (is_symbol(middle_right)) {
-      return 1;
-    }
+int number_extract_idxs(int idx, char *str) {
+  int idxes[2] = {-1, -1};
+  char byte = str[idx];
+  if (!isdigit(byte)) {
+    return 0;
   }
 
-  char *above_row = strlen(rows[0]) > 0 ? rows[0] : NULL;
-  char *under_row = strlen(rows[2]) > 0 ? rows[2] : NULL;
-
-  for (int i = start; i <= end; i++) {
-    if (above_row != NULL) {
-      char above_byte = above_row[i];
-      if (is_symbol(above_byte)) {
-        return 1;
+  idxes[0] = idx;
+  idxes[1] = idx;
+  int finished_dir[2] = {0, 0};
+  while (!(finished_dir[0] && finished_dir[1])) {
+    if (!finished_dir[0]) {
+      char left_byte = str[idxes[0] - 1];
+      if (idxes[0] == 0) {
+        finished_dir[0] = 1;
+      } else if (isdigit(left_byte)) {
+        idxes[0]--;
+      } else {
+        finished_dir[0] = 1;
       }
     }
-    if (under_row != NULL) {
-      char under_byte = under_row[i];
-      if (is_symbol(under_byte)) {
-        return 1;
+
+    if (!finished_dir[1]) {
+      char right_byte = str[idxes[1] + 1];
+      if (idxes[1] == strlen(str)) {
+        finished_dir[1] = 1;
+      } else if (isdigit(right_byte)) {
+        idxes[1]++;
+      } else {
+        finished_dir[1] = 1;
       }
     }
   }
-  return 0;
+
+  int len = idxes[1] - idxes[0] + 1;
+  char num_str[len + 1];
+  strncpy(num_str, str + idxes[0], len);
+  num_str[len] = '\0';
+  return atoi(num_str);
 }
 
 void center_row_process(char rows[3][1024], int *sum) {
   char *row = rows[1];
   for (int i = 0; i < strlen(row); i++) {
     char byte = row[i];
-    if (isdigit(byte)) {
-      int num_start_idx = i;
-      while (isdigit(byte)) {
-        byte = row[++i];
+    if (byte == '*') {
+      int gears[2] = {-1, -1};
+
+      int num = 0;
+      // middle left
+      if ((num = number_extract_idxs(i - 1, rows[1])) != 0) {
+        if (gears[0] == -1) {
+          gears[0] = num;
+        } else if (gears[1] == -1) {
+          gears[1] = num;
+        }
       }
 
-      char num_str[i - num_start_idx];
-      strncpy(num_str, row + num_start_idx, i - num_start_idx);
-      num_str[i - num_start_idx] = '\0';
-      if (is_adjacent_to_symbol(num_start_idx, i - 1, rows)) {
-        *sum += atoi(num_str);
+      // middle right
+      if ((num = number_extract_idxs(i + 1, rows[1])) != 0) {
+        if (gears[0] == -1) {
+          gears[0] = num;
+        } else if (gears[1] == -1) {
+          gears[1] = num;
+        }
+      }
+
+      // top middle
+      if ((num = number_extract_idxs(i, rows[0])) != 0) {
+        if (gears[0] == -1) {
+          gears[0] = num;
+        } else if (gears[1] == -1) {
+          gears[1] = num;
+        } else {
+          continue;
+        }
+      } else {
+        // top left
+        if ((num = number_extract_idxs(i - 1, rows[0])) != 0) {
+          if (gears[0] == -1) {
+            gears[0] = num;
+          } else if (gears[1] == -1) {
+            gears[1] = num;
+          } else {
+            continue;
+          }
+        }
+        // top right
+        if ((num = number_extract_idxs(i + 1, rows[0])) != 0) {
+          if (gears[0] == -1) {
+            gears[0] = num;
+          } else if (gears[1] == -1) {
+            gears[1] = num;
+          } else {
+            continue;
+          }
+        }
+      }
+
+      // bottom middle
+      if ((num = number_extract_idxs(i, rows[2])) != 0) {
+        if (gears[0] == -1) {
+          gears[0] = num;
+        } else if (gears[1] == -1) {
+          gears[1] = num;
+        } else {
+          continue;
+        }
+      } else {
+        // bottom left
+        if ((num = number_extract_idxs(i - 1, rows[2])) != 0) {
+          if (gears[0] == -1) {
+            gears[0] = num;
+          } else if (gears[1] == -1) {
+            gears[1] = num;
+          } else {
+            continue;
+          }
+        }
+        // bottom right
+        if ((num = number_extract_idxs(i + 1, rows[2])) != 0) {
+          if (gears[0] == -1) {
+            gears[0] = num;
+          } else if (gears[1] == -1) {
+            gears[1] = num;
+          } else {
+            continue;
+          }
+        }
+      }
+
+      if (gears[0] != -1 && gears[1] != -1) {
+        *sum += gears[0] * gears[1];
       }
     }
   }
